f34da4e1355efd1c878ddcc04710ca9febc86335
Narciso Benigno <narciso.benigno@gmail.com>
2024-10-26 16:57:26 +1100

    feat(oss): build id from a string

--- service/app/src/test/kotlin/jtodo/oss/es/IdTest.kt
+++ service/app/src/test/kotlin/jtodo/oss/es/IdTest.kt
@@ -2,6 +2,7 @@ package jtodo.oss.es
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class IdTest {
     @Test
@@ -19,5 +20,24 @@ class IdTest {
             Id.scoped("scope", "value"),
             Id.scoped("another-scope", "value")
         )
+    }
+
+    @Test
+    fun `builds fixed string`() {
+        assertEquals(
+            Id.fixed("value"),
+            Id.fixed("value")
+        )
+        assertEquals(
+            Id.fixed("value"),
+            Id.fixed(" value    ")
+        )
+
+        assertNotEquals(
+            Id.fixed("value"),
+            Id.fixed("another value")
+        )
+        assertThrows<IllegalArgumentException>("value cannot be empty") { Id.fixed("") }
+        assertThrows<IllegalArgumentException>("value cannot be empty") { Id.fixed("    ") }
     }
 }
--- service/oss/src/main/kotlin/jtodo/oss/es/Id.kt
+++ service/oss/src/main/kotlin/jtodo/oss/es/Id.kt
@@ -7,5 +7,10 @@ data class Id(private val value: UUID) {
         fun scoped(scope: String, value: String): Id {
             return Id(UUID.nameUUIDFromBytes((scope + value).encodeToByteArray()))
         }
+
+        fun fixed(value: String): Id {
+            require(value.trim().isNotEmpty()) { "value cannot be empty" }
+            return Id(UUID.nameUUIDFromBytes(value.trim().encodeToByteArray()))
+        }
     }
 }
